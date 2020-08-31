8264419dd833098a75dda06aa351c452ca319eef
Kayla Ross <kaylakross13@gmail.com>
2020-08-30 21:57:16 -0400

    more

--- index.js
+++ index.js
@@ -224,10 +224,9 @@ artists["name"] = "Vincent Van Gogh";
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    return "The artist at index {id} is {name}";
   }
   
-  /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
